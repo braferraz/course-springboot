9b2666e4481fbad7e29cfb0fac9327d276ecda15
Braian Ferraz <braianfs2009@gmail.com>
2020-08-18 09:13:18 -0300

    Service layer, component registration

--- src/main/java/com/braferraz/course/resources/UserResource.java
+++ src/main/java/com/braferraz/course/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.braferraz.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.braferraz.course.entities.User;
+import com.braferraz.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-	User user = new User(1L, "Braian","braian@gmail.com","999999999","123123123");
-	return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+	return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
