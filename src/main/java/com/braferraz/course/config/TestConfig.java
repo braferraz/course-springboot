20f71ab8e0cf403b149b7482aba529c28ffc7726
Braian Ferraz <braianfs2009@gmail.com>
2020-08-27 14:35:06 -0300

    Payment, one-to-one association

--- src/main/java/com/braferraz/course/config/TestConfig.java
+++ src/main/java/com/braferraz/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.braferraz.course.entities.Category;
 import com.braferraz.course.entities.Order;
 import com.braferraz.course.entities.OrderItem;
+import com.braferraz.course.entities.Payment;
 import com.braferraz.course.entities.Product;
 import com.braferraz.course.entities.User;
 import com.braferraz.course.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner{
 	
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);		
 	}
 }
